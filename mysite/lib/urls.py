729f43ace12b5cd80f80fccc34caddc6ccf15b28
jiaming9844 <jiaming9844@163.com>
2020-02-26 17:09:47 +0800

    update

--- mysite/lib/urls.py
+++ mysite/lib/urls.py
@@ -1,6 +1,12 @@
 # lib/urls.py
 from django.urls import path
 from . import views
+
+app_name = 'lib'
+
 urlpatterns = [
         path('', views.index, name='index'),
+        path('addBook/', views.addBook, name='addBook'),
+        path('detail/', views.detail,name='detail'),
+        path('delBook/<int:book_id>', views.deleteBook, name='delBook'),
         ]
